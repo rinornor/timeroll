b5ff1c56e33593f4e549dbbd40e02d7b31d618a8
rinornor <rinornor@gmail.com>
2023-11-07 22:37:36 +0100

    Redesigned create tab

--- resources/views/appearances/create.blade.php
+++ resources/views/appearances/create.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @if ($errors->any())
+            @if ($errors->any())
                     <div class="alert alert-danger">
                         <ul>
                             @foreach ($errors->all() as $error)
@@ -17,40 +17,38 @@
                         </ul>
                     </div>
                  @endif
-                <form action="{{route('appearance.store')}}" method="POST"> 
-                    @csrf
-                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div class="sm:col-span-4">
-                            <label for="time_in" class="block text-sm font-medium leading-6 text-gray-900">Start</label>
-                            <div class="mt-2">
-                                <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    <input type="datetime-local" name="time_in" id="time_in" autocomplete="time_in" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="12:00">
-                                </div>
-                            </div>
-                        </div>
-                        
-                            <div class="sm:col-span-4">
-                                <label for="time_out" class="block text-sm font-medium leading-6 text-gray-900">Finish</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <input type="datetime-local" name="time_out" id="time_out" autocomplete="time_out" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="18:00">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="sm:col-span-4">
-                                <label for="date" class="block text-sm font-medium leading-6 text-gray-900">Date</label>
-                                <div class="mt-2">
-                                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <input type="date" name="date" id="date" autocomplete="date"  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
-                                    </div>
-                                </div>
-                            </div>
-                        
-                        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>                       
-                         
-                        
-                    </div>
-                </form>
+
+            <form class="w-full max-w-lg" action="{{route('appearance.store')}}" method="POST">
+            @csrf
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="time_in">
+        Start
+      </label>
+      <input name="time_in" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="time_in" type="datetime-local">
+      
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="time_out">
+        Finish
+      </label>
+      <input name="time_out" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="time_out" type="datetime-local">
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="date">
+        Date
+      </label>
+      <input name="date" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="date" type="date">
+      
+    </div>
+  </div>
+  <div class="flex flex-wrap w-1/6 mb-4">
+  <button  type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>                       
+
+  </div>
+</form>
                 
             </div>
             
